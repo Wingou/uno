da08553eed234cfb5630a06600840688a240a6c4
Wingou <wing92300@gmail.com>
2019-11-24 10:50:18 +0100

    remove useless import

--- src/Main.elm
+++ src/Main.elm
@@ -1,21 +1,12 @@
 module Main exposing (..)
 
-import Asset exposing (ImageString, imgAvatarBorder1, imgStars, panelAvatars, path, pathFilename, spriteAvatars, spriteCards, src)
-import Basics exposing (round)
 import Browser
-import Debug exposing (log)
-import Html exposing (Html, a, b, br, button, div, h1, h2, h3, h4, h5, hr, img, input, span, text)
-import Html.Attributes exposing (placeholder, style, title)
-import Html.Events exposing (onClick, onInput)
-import List exposing (append, concat, drop, filter, head, indexedMap, isEmpty, length, map, map2, range, repeat, reverse, sortBy, sum, tail, take)
-import Random
-import String exposing (fromInt)
-import Tuple
+import Html exposing (Html)
 import Types exposing (..)
 import Update exposing (..)
 import Views.Avatar exposing (..)
 import Views.GameOver exposing (..)
-import Views.Home exposing (homeView)
+import Views.Home exposing (..)
 import Views.Playing exposing (..)
 
 
